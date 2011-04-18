8785a02223df4483c74dd2e7d0b4670f732d96c0
Patrick O'Leary <patrick.oleary@gmail.com>
2011-04-17 23:03:30 -0500

    [Haskell] Partial implementation of cagematch mode.

--- haskell/src/Main.hs
+++ haskell/src/Main.hs
@@ -16,14 +16,20 @@ module Main (
     main
 ) where
 
+import Control.Applicative
+
 import Data.Char
 import Data.List
+import Data.Either
 import Data.Function
 
 import System.Environment
 
 import qualified Data.ByteString.Char8 as C
 import qualified Crypto.Hash.SHA1 as H
+
+data Victory = Victory { winner :: String, loser :: String } deriving (Show)
+type Outcome = Either String Victory
 
 main :: IO ()
 main = do
@@ -33,18 +39,28 @@ main = do
 salt = ".NaCl.$^d43lwz;)3s.optimize.this"
 
 -- Perform a complete battle between monsters
-battle monsters = printWinner $ getWinner $ zip (map hash $ mangle monsters) monsters
+battle = printWinner . getWinner . battlepair
 
-printWinner :: Either String String -> String
+battlepair :: [String] -> [(C.ByteString, String)]
+battlepair monsters =
+    let tm = map trim monsters
+    in zip (map hash $ mangle tm) tm
+
+printWinner :: Outcome -> String
 printWinner =
     either (\w -> "The " ++ w ++ " refuses to fight itself!")
-           (\w -> "The " ++ w ++ " wins!")
+           (\w -> "The " ++ winner w ++ " wins!")
 
-getWinner :: [(C.ByteString, String)] -> Either String String
+getWinner :: [(C.ByteString, String)] -> Outcome
 getWinner entries
-    | all (== fst (head entries)) (map fst entries) = Left $ firstNameFrom entries
-    | otherwise = Right $ firstNameFrom $ sortBy (compare `on` fst) entries
-    where firstNameFrom = trim . snd . head
+    | all (== fst (head entries)) (map fst entries) = Left $ snd $ head $ entries
+    | otherwise = Right $ Victory (head fight) (head $ tail fight)
+    where fight = snd $ unzip $ sortBy (compare `on` fst) entries
+
+cagematch :: [String] -> [Victory]
+cagematch ms =
+    let cm x y = getWinner $ battlepair [x, y]
+    in rights $ cm <$> ms <*> ms
 
 -- String manipulations
 hash :: String -> C.ByteString
