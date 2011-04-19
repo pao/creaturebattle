aeeed601d33bdc3fc2aa0dbc2ae042f8f240ba5a
Patrick O'Leary <patrick.oleary@gmail.com>
2011-04-18 22:46:35 -0500

    [Haskell] Support command line option for cagematch. Deduplicated battles.

--- haskell/src/Main.hs
+++ haskell/src/Main.hs
@@ -17,26 +17,49 @@ module Main (
 ) where
 
 import Control.Applicative
+import Control.Monad
 
 import Data.Char
-import Data.List
 import Data.Either
 import Data.Function
+import Data.List
 
 import qualified Data.Graph.Inductive as G
 
+import System.Console.GetOpt
 import System.Environment
 
 import qualified Data.ByteString.Char8 as C
 import qualified Crypto.Hash.SHA1 as H
 
-data Victory = Victory { winner :: String, loser :: String } deriving (Show)
+data Victory = Victory { winner :: String, loser :: String } deriving (Show, Eq)
 type Outcome = Either String Victory
+
+-- http://www.haskell.org/haskellwiki/High-level_option_handling_with_GetOpt
+data Options = Options { optCagematch :: Bool }
+
+startOptions :: Options
+startOptions = Options { optCagematch = False }
+
+options :: [OptDescr (Options -> IO Options)]
+options =
+    [ Option "c" ["cagematch"]
+        (NoArg
+            (\opt -> return opt { optCagematch = True }))
+        "Run cagematch instead of direct competition."
+    ]
 
 main :: IO ()
 main = do
-    args <- getArgs
-    putStrLn $ battle $ take 2 args
+    rawArgs <- getArgs
+
+    let (actions, args, errors) = getOpt RequireOrder options rawArgs
+    opts <- foldl (>>=) (return startOptions) actions
+
+    let Options { optCagematch = doCagematch } = opts
+
+    when doCagematch $ putStrLn $ G.graphviz (graphCM args (cagematch args)) "" (0,0) (0,0) G.Portrait
+    when (not doCagematch) $ putStrLn $ battle $ take 2 args
 
 salt = ".NaCl.$^d43lwz;)3s.optimize.this"
 
@@ -62,7 +85,7 @@ getWinner entries
 cagematch :: [String] -> [Victory]
 cagematch ms =
     let cm x y = getWinner $ battlepair [x, y]
-    in rights $ cm <$> ms <*> ms
+    in nub $ rights $ cm <$> ms <*> ms
 
 graphCM :: [String] -> [Victory] -> G.Gr String ()
 graphCM ms vs =
