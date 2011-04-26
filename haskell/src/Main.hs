34e76a39cee16d582310bc1bde7b066fdf91685f
Patrick O'Leary <patrick.oleary@gmail.com>
2011-04-25 23:09:36 -0500

    [Haskell] Implementation of Tournament mode, with output from Show for now, with massive internal refactoring.

--- haskell/src/Main.hs
+++ haskell/src/Main.hs
@@ -74,30 +74,45 @@ main = do
     opts <- foldl (>>=) (return defaultOpts) actions
 
     case optBattle opts of
-        Single      -> putStrLn $ printBattle $ take 2 args
-        Cage        -> matchgraph cagematch args
-        Tournament  -> return ()
+        Single      -> putStrLn $ printWinner $ battle (head args) (args !! 1)
+        Cage        -> printGraph $ mkBattleGraph $ cagematch args
+        Tournament  -> print $ tournament $ initTournament args
 
 salt = ".NaCl.$^d43lwz;)3s.optimize.this"
 
 -- Single battle functions
-printBattle = printWinner . battle
-
-battle = getWinner . battlepair
-
 printWinner :: Outcome -> String
 printWinner =
     either (\w -> "The " ++ w ++ " refuses to fight itself!")
            (\w -> "The " ++ winner w ++ " wins!")
 
--- Multi-battle functions
+-- Cagematch functions
 matchgraph :: ([String] -> [Victory]) -> [String] -> IO ()
-matchgraph f ms = printGraph . mkBattleGraph ms $ f ms
+matchgraph f ms = printGraph . mkBattleGraph $ f ms
 
 cagematch :: [String] -> [Victory]
 cagematch ms =
-    let cm x y = getWinner $ battlepair [x, y]
-    in nub $ rights $ cm <$> ms <*> ms
+    (rights . nub) $ battle <$> ms <*> ms
+
+-- Tournament functions
+initTournament :: [String] -> [Victory]
+initTournament = map (\x -> Victory x x)
+
+tournament :: [Victory] -> [[Victory]]
+tournament []  = []
+tournament [x] = []
+tournament xs  =
+    let
+        round = tRound xs
+    in round : tournament round
+
+tRound :: [Victory] -> [Victory]
+tRound = map (either (\x -> Victory x x) id . (uncurry battle)) . pairify . map winner
+
+pairify :: [a] -> [(a, a)]
+pairify []        = []
+pairify [x]       = [(x, x)]
+pairify (x:x':xs) = (x, x') : pairify xs
 
 -- Graph generation functions
 printGraph = graphOut . dotgraph
@@ -113,33 +128,30 @@ dotgraph = GV.graphToDot params
         params = GV.nonClusteredParams { GV.fmtNode = label }
         label (_, s) = [GV.Label $ GV.StrLabel s]
 
-mkBattleGraph :: [String] -> [Victory] -> G.Gr String ()
-mkBattleGraph ms vs =
-    let edges = map (\v -> (loser v, winner v, ()))
+mkBattleGraph :: [Victory] -> G.Gr String ()
+mkBattleGraph vs =
+    let
+        edges = map (\v -> (loser v, winner v, ()))
+        ms = nub $ map loser vs ++ map winner vs
     in fst $ G.mkMapGraph ms (edges vs)
 
 -- General battle functions
-battlepair :: [String] -> [(C.ByteString, String)]
-battlepair monsters =
-    let tm = map trim monsters
-    in zip (map hash $ mangle tm) tm
-
-getWinner :: [(C.ByteString, String)] -> Outcome
-getWinner entries
-    | all (== fst (head entries)) (map fst entries) = Left $ snd $ head $ entries
-    | otherwise = Right $ Victory (head fight) (fight !! 1)
-    where fight = snd $ unzip $ sortBy (compare `on` fst) entries
+battle :: String -> String -> Outcome
+battle x y
+    | x == y    = Left x
+    | x' > y'   = Right $ Victory {winner = trim x, loser = trim y}
+    | otherwise = Right $ Victory {winner = trim y, loser = trim x}
+    where
+        cx = clean x
+        cy = clean y
+        x' = hash $ cx ++ cy ++ salt
+        y' = hash $ cy ++ cx ++ salt
 
 -- String manipulations
 hash :: String -> C.ByteString
 hash = H.hash . C.pack
 
-mangle :: [String] -> [String]
-mangle xs =
-    let cxs = map clean xs
-    in map (++salt) $ zipWith (++) cxs (reverse cxs)
-
-clean = trim . map toLower
+clean = map toLower . trim
 
 trim :: String -> String
 trim =
